0c647f6f9fda778e21cf6a979442b0efd3ce78d8
Sohail103 <sohailraj.satapathy@gmail.com>
2025-09-07 15:14:09 +0530

    simulating phosphor fade with surfaces

--- src.py
+++ src.py
@@ -11,12 +11,17 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Oscilloscope screen")
 clock = pygame.time.Clock()
 
+fade_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+fade_surface.set_alpha(15)
+fade_surface.fill((0,0,0))
+
 # main loop
 
 running = True
 
-angle=0
-radius=100
+angle = 0
+amplitude = 200
+frequency = 10
 
 while running:
 
@@ -28,14 +33,18 @@ while running:
     #y = int(CENTER_Y + radius*math.sin(math.radians(angle)))
     #angle += 1
 
-    x = int(256*math.sin(math.radians(clock.get_time()))) + CENTER_X
+    screen.blit(fade_surface, (0,0))
+
+    x = CENTER_X + amplitude * math.sin(math.radians(frequency * angle))
     y = CENTER_Y
 
     #screen.fill((0,0,0))        # background
 
-    screen.set_at((x,y), (255,255,255))
+    #screen.set_at((x,y), (255,255,255))
+    pygame.draw.circle(screen, (255,255,255), (x,y), 2)
+    angle += 0.1
     
     pygame.display.flip()       # updates screen
-    clock.tick(60)             # 60 fps
+    clock.tick(60)              # 60 fps
 
 pygame.quit()
