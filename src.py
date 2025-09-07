01f87453fb08c3e89d62fa84cb9d2d6c0e333acd
Sohail103 <sohailraj.satapathy@gmail.com>
2025-09-07 20:17:41 +0530

    drawing square

--- src.py
+++ src.py
@@ -18,14 +18,14 @@ fade_surface.fill((0,0,0))
 
 l1stat = l2stat = l3stat = l4stat = 0
 
-(l1x1, l1y1) = (-1, 1)
-(l1x2, l1y2) = (1, 1)
-(l2x1, l2y1) = (1, 1)
-(l2x2, l2y2) = (1, -1)
-(l3x1, l3y1) = (1, -1)
-(l3x2, l3y2) = (-1, -1)
-(l4x1, l4y1) = (-1, -1)
-(l4x2, l4y2) = (-1, 1)
+(l1x1, l1y1) = (-100 + CENTER_X, 100 + CENTER_Y)
+(l1x2, l1y2) = (100 + CENTER_X, 100 + CENTER_Y)
+(l2x1, l2y1) = (100 + CENTER_X, 100 + CENTER_Y)
+(l2x2, l2y2) = (100 + CENTER_X, -100 + CENTER_Y)
+(l3x1, l3y1) = (100 + CENTER_X, -100 + CENTER_Y)
+(l3x2, l3y2) = (-100 + CENTER_X, -100 + CENTER_Y)
+(l4x1, l4y1) = (-100 + CENTER_X, -100 + CENTER_Y)
+(l4x2, l4y2) = (-100 + CENTER_X, 100 + CENTER_Y)
 
 step = 0.01
 xstep = ystep = 0
@@ -44,14 +44,41 @@ while running:
     if (l1stat==0):
         xstep = (l1x2-l1x1)*step
         ystep = (l1y2-l1y1)*step
+        
+        if((x>l1x2) & (y>l1y2)):
+            l1stat += 1
+    
+    else if (l2stat==0):
+        xstep = (l2x2-l2x1)*step
+        ystep = (l2y2-l2y1)*step
+        
+        if((x >= l2x2) & (y >= l2y2)):
+            l2stat += 1
+    
+    elif (l3stat==0):
+        xstep = (l3x2-l3x1)*step
+        ystep = (l3y2-l3y1)*step
+        
+        if((x >= l3x2) & (y >= l3y2)):
+            l3stat += 1
+
+    elif (l4stat==0):
+        xstep = (l4x2-l4x1)*step
+        ystep = (l4y2-l4y1)*step
+        
+        if((x >= l4x2) & (y >= l4y2)):
+            l4stat += 1
 
     pygame.draw.circle(screen, (255,255,255), (x,y), 2)
 
-    if (x < l1x2 && y < l1y2):
+    if ~(l1stat & l2stat & l3stat & l4stat):
         x += xstep
         y += ystep
     
+    if (l1stat == l2stat == l3stat == l4stat & l1stat !=0 ):
+        l1stat = l2stat = l3stat = l4stat = 0
+    
     pygame.display.flip()       # updates screen
-    clock.tick(1000)              # 60 fps
+    clock.tick(60)              # 60 fps
 
 pygame.quit()
